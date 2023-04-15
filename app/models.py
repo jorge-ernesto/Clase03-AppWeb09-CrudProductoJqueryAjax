fa701e68539ff2db0ee8893b43aab2e19880aa7b
Ernesto <jorge.cywdt@gmail.com>
2023-04-15 10:11:24 -0500

    Notas para entender mejor el codigo

--- app/models.py
+++ app/models.py
@@ -2,11 +2,14 @@ from django.db import models
 
 # Create your models here.
 
-
-class Producto(models.Model):
-    # definir campos
+class Producto(models.Model):  # Modelo creado para ejecutar migraciones
+    # Definir los datos de la clase (campos de la tabla)
+    # Si no se especifica lo contrario, todos los campos seran requeridos por defecto, es decir NOT NULL
     producto = models.CharField(max_length=100)
     fecha = models.DateField()
     categoria = models.CharField(max_length=1)
     precio = models.DecimalField(max_digits=6, decimal_places=2)
     stock = models.IntegerField()
+
+    def __str__(self):
+        return str(self.__dict__)
--- app/views.py
+++ app/views.py
@@ -1,56 +1,56 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .models import Producto
 
 # Create your views here.
 
-
 def index(request):
-    return render(request,"app/index.html")
+    return render(request, "app/index.html")
+
+def read(request):
+    productos = Producto.objects.all()
+
+    print('productos', productos)
+    # Modificar el formato de fecha a 'yyyy-mm-dd'
+    for p in productos:
+        p.fecha = '{:%Y-%m-%d}'.format(p.fecha)
+    #cerrar
+    print('productos', productos)
+
+    contexto = {'productos':productos}
+
+    return render(request, 'app/result.html', contexto)
 
 def create(request):
     # crear objeto producto de la clase producto
-    producto=Producto(producto=request.POST["producto"],
-                    categoria=request.POST["categoria"],
-                    fecha=request.POST["fecha"],
-                    precio=request.POST["precio"],
-                    stock=request.POST["stock"])
+    producto = Producto(producto = request.POST["producto"],
+                    categoria = request.POST["categoria"],
+                    fecha = request.POST["fecha"],
+                    precio = request.POST["precio"],
+                    stock = request.POST["stock"])
     # grabar producto en la BD
     producto.save()
     return redirect('/')
 
-def read(request):
-    productos=Producto.objects.all()
-    #modificando el formato de fecha
-    for p in productos:
-        p.fecha='{:%d-%m-%Y}'.format(p.fecha)
+def edit(request, id):
+    producto = Producto.objects.get(id = id)
+    fe = producto.fecha
+    producto.fecha = '{:%Y-%m-%d}'.format(fe)
+    contexto = {'producto':producto}
+    return render(request, 'app/edit.html', contexto)
 
-    contexto={'productos':productos}
-    
-    return render(request,'app/result.html',contexto)
-
-def delete(request,id):
-    producto=Producto.objects.get(id=id)
-    producto.delete()
-    return redirect('/')
-
-def edit(request,id):
-    producto=Producto.objects.get(id=id)
-    fe=producto.fecha
-    producto.fecha='{:%Y-%m-%d}'.format(fe)
-    contexto={'producto':producto}
-    return render(request,'app/edit.html',contexto)
-
-def update(request,id):
-    producto=Producto.objects.get(id=id)
+def update(request, id):
+    producto = Producto.objects.get(id = id)
     # actualizar datos
-    producto.producto=request.POST["producto"]
-    producto.categoria=request.POST["categoria"]
-    producto.fecha=request.POST["fecha"]
-    producto.precio=request.POST["precio"]
-    producto.stock=request.POST["stock"]
+    producto.producto = request.POST["producto"]
+    producto.categoria = request.POST["categoria"]
+    producto.fecha = request.POST["fecha"]
+    producto.precio = request.POST["precio"]
+    producto.stock = request.POST["stock"]
     # graba producto actualizado
     producto.save()
     return redirect('/')
 
-
-
+def delete(request, id):
+    producto = Producto.objects.get(id = id)
+    producto.delete()
+    return redirect('/')
